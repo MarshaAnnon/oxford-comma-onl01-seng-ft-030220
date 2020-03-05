a6a7c8eb053fb39fa5d593bb4fa05883dd396c57
Marsha Annon <marshaannon@gmail.com>
2020-03-05 15:01:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   array = 3
   if array(1)
     return array.join
-  elsif array(2)
+  else array(2)
     return array.join("and")
-  else
-    return array.join("and") << ,[2]
   end
 end
